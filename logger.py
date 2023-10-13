1b3ed64e53f47feb19ea3328cf1a3e1e6c080d47
datenlord1510 <datenlord1510@protonmail.com>
2023-10-13 14:04:41 +0200

    removed comments

--- logger.py
+++ logger.py
@@ -2,11 +2,9 @@ import logging
 
 
 def setup_logger(logger_name, log_file="LOGS.log"):
-    # Create a logger
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
 
-    # Create a formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     file_handler = logging.FileHandler(log_file)
